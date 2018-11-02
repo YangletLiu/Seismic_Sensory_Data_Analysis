9759d27f68a6ff1301cb869d8383618423b84a7c
Xiao-Yang Liu <xl2427@columbia.edu>
2018-11-02 19:53:47 +0800

    Add files via upload

--- error/Tubal_TNN.m
+++ error/Tubal_TNN.m
@@ -1,38 +1,44 @@
 clc; 
 clear all; 
 close all;
-% profile ong
+
 
 %% data loading
-load('T_synthetic_tuabl_rank_2.mat');   %加载我们自己合成的人工合成数据;
+% load('T_synthetic_tuabl_rank_2.mat');   %加载我们自己合成的人工合成数据;
+load('volume.mat');  %数据大小：326*431*531
+T = volume(1:300,1:120,1:80);
+% T = permute(T,[3,1,2]);
+
+%% 数据预处理
+original_tubalRank = LowTubalCDF(T,1);
+[U,S,V] = tSVDs(T,15);
+temp = tprod(U,S);
+T_test = tprod(temp,V);
+T_test_RSE = norm(T_test(:) - T(:))/ norm(T(:));
+
+original_tubalRank1 = LowTubalCDF(T_test,1);
+T1 = T_test;
 
 
-% load('volume.mat');%真实地震数据
-% T = volume(301:326,1:20,1:20);
 
-
-
-szT = size(T);   
-tubalRank = LowTubalCDF(T, 1);
-
-
-srs = [0.05 : 0.05 : 0.2];
+srs =[0.02 : 0.02 : 0.2];
 sampling_rse = zeros(2, length(srs));
-T1 = permute(T,[3,1,2]);
+O_sampling_rse = zeros(2, length(srs));
+
+szT1 = size(T1);   
 
 %% 调节tubal-rank
-tubalRank2 = LowTubalCDF(T1, 1);
-r = tubalRank2;
+r = 15;
 
 
-for i = 1 : 10
+for i = 1 : 3
 for loop = 1 : length(srs)
     samplingrate = srs(loop);
-    MatOmega1 = randsample([0 1],szT(3),true,[samplingrate, 1-samplingrate]);
-    omega = ones(szT(1),szT(2),szT(3));
-    for k=1:szT(3)
+    MatOmega1 = randsample([0 1],szT1(3),true,[samplingrate, 1-samplingrate]);
+    omega = ones(szT1(1),szT1(2),szT1(3));
+    for k=1:szT1(3)
         if(MatOmega1(k)==0)
-           omega(:,:,k)=zeros(szT(1),szT(2));
+           omega(:,:,k)=zeros(szT1(1),szT1(2));
         end
     end
     
@@ -40,6 +46,7 @@ for loop = 1 : length(srs)
 %                    Tubal-Alt-Min                         %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 RSE = zeros(1,2); 
+OriginalRSE = zeros(1,2);
 %% observations
 [m,n,k] = size(T1);
 T_f = fft(T1, [], 3);
@@ -69,7 +76,7 @@ omega_f = fft(omega, [], 3);
 
 
 
-   maxiter = 20;
+   maxiter = 15;
    TubalAMCurve = zeros(maxiter, 1);
    iter=1;
    while iter <=maxiter
@@ -91,6 +98,7 @@ omega_f = fft(omega, [], 3);
        Y_est = ifft(Y_f, [], 3);
        T_est = tprod(X_est, Y_est);
        RSE(1) =  norm(T_est(:) - T1(:)) / norm(T1(:));
+       OriginalRSE(1) =  norm(T_est(:) - T(:)) / norm(T(:));
        TubalAMCurve(iter) = RSE(1);
        iter = iter + 1;
    end
@@ -119,24 +127,27 @@ omega_f = fft(omega, [], 3);
     tnnT               =        reshape(tnnT,[n1,n2,n3])         ;
    TNNQ1 = -20*log10(norm(tnnT(:) - T1(:)) / norm(T1(:)));
    RSE(2) =  norm(tnnT(:) - T1(:)) / norm(T1(:));
+   OriginalRSE(2) =  norm(tnnT(:) - T(:)) / norm(T(:));
    fprintf('***********************RSE = %d ***********\n',RSE(2)); 
    
    sampling_rse(:, loop) = sampling_rse(:, loop) + RSE(:);
+   O_sampling_rse(:, loop) = O_sampling_rse(:, loop) + OriginalRSE(:);
 end
 end
-sampling_rse = sampling_rse ./ 10;
-
+sampling_rse = sampling_rse ./ 3;
+O_sampling_rse = O_sampling_rse ./ 3;
 
 % 绘制收敛速度
-len = min([length(TubalAMCurve), length(tnnCurve)]);
-figure;semilogy(TubalAMCurve(1:len), 'o-'); title(['Convergence Speed ']);
-hold on; semilogy(tnnCurve(1:len), '*-');
+len = max([length(TubalAMCurve), length(tnnCurve)]);
+figure;semilogy(TubalAMCurve(1:length(TubalAMCurve)), 'o-'); title(['Convergence Speed ']);
+hold on; semilogy(tnnCurve(1:length(tnnCurve)), '*-');
 legend('Tubal-Alt-Min', 'TNN'); 
 xlabel('Iteration');ylabel('RSE in log-scale');
 grid on;
 
 % 重构误差
-figure;semilogy([0.05 : 0.05 : 0.2]*100, sampling_rse(1,:), '+-'); title(['Reconstruction Error']);
-hold on; semilogy([0.05 : 0.05 : 0.2]*100, sampling_rse(2, :), '*-');
+figure;plot([0.98: -0.02 : 0.8]*100, sampling_rse(1,:), '+-'); title(['Reconstruction Error']);
+hold on; plot([0.98: -0.02 : 0.8]*100, sampling_rse(2, :), '*-');
 legend( 'Tubal-Alt-Min','TNN'); 
-xlabel('Slice Missing Rate %');ylabel('RSE in log-scale');
+xlabel('Slice Sampling Rate * 100 (%)');ylabel('RSE');
+grid on;
