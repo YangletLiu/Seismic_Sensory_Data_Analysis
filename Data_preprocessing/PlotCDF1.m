a19812f339784b394190ee7b16f7e93585df13d0
Xiao-Yang Liu <xl2427@columbia.edu>
2018-11-08 20:10:10 +0800

    Update PlotCDF1.m

--- Data_preprocessing/PlotCDF1.m
+++ Data_preprocessing/PlotCDF1.m
@@ -1,6 +1,6 @@
 function PlotCDF1(CDF, dimensional)
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
+%UNTITLED 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
 
 str = {};
 sz = length(CDF(1, :));
@@ -8,12 +8,7 @@ figure;
 markers = ['-o'; '-+'; '-*'; '-s'; '-d'; '->'; '-p'; '-h'];
 for i = 1 : size(CDF, 1)
     plot((1:sz)/sz * 100, CDF(i, :), markers(i, :) ,'MarkerSize', 4);hold on; 
-%     if i <= length(dimensional)
-%         str = [str; ['along ', num2str(i), '-th dimensional FFT transform' ]];
-%     end
 end
-% grid on; plot((1:sz)/sz * 100, CDF(4, :), markers(4, :) ,'MarkerSize', 4);hold on;
-% str = [str; ['No FFT transform']];
 grid on; title('Empirical CDF'); xlabel('Top portion of singular values *100(%)'); ylabel('CDF');
 legend('original data','processed data');
 end
