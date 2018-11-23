2dfd4614a51fc66bf3ec4e87554c65ebbd51d81c
woshifyq <964802594@qq.com>
2018-11-23 09:56:23 +0800

    Update circdiag.m

--- circdiag.m
+++ circdiag.m
@@ -1,4 +1,7 @@
-%P�ĺ�����Ƭ��ÿһ��tube��ѭ�����������Խ�
+%先对P的一个侧向切片的每一个tube做循环矩阵构成新tensorB，再对B的每个tube做对角阵构成新矩阵
+% input ：P：n1 x 1 X n3  （实际为P的一个侧向切片）
+% output：A：（n1xn3）x （n1xn3）
+ 
 
 function [A]=circdiag(P)
 [n1,~,n3]=size(P);
